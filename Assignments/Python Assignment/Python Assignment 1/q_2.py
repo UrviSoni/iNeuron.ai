b39b8d0444a0d7d6aeaa18952b081680bddbf69d
UrviSoni <connectwithurvisoni@gmail.com>
2020-12-03 23:03:32 +0530

    [ADD] python_assignments: q_2

---
+++ Assignments/Python Assignment/Python Assignment 1/q_2.py
@@ -0,0 +1,6 @@
+# Write a Python program to accept the user's first and last name and then getting them
+# printed in the the reverse order with a space between first name and last name.
+
+fname = input("Your First Name: ") # Taking 'fname' as an input
+lname = input("Your Last Name: ") # Taking 'lname' as an input
+print(lname + " " + fname) # printing them in the reverse order with a space between 'fname' and 'lname'
