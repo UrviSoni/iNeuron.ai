a9417fc68b717bea6530e38a4ec1095e76d87faa
UrviSoni <connectwithurvisoni@gmail.com>
2020-12-03 23:11:07 +0530

    [ADD] python_assignments: q_3

---
+++ Assignments/Python Assignment/Python Assignment 1/q_3.py
@@ -0,0 +1,8 @@
+# Write a Python program to find the volume of a sphere with diameter 12 cm.
+# Formula: V=4/3 * π * r 3
+
+pi = 3.14 # Taking pi = 3.14 value or you can use (22/7)
+r = 6 # Here in the question they tell diameter = 12 cm so radious = (d/2) = 6 cm
+V = 4/3*pi* r**3 # Volume Formula
+
+print("The Volume of a sphere is: ", V) # printing the volume
